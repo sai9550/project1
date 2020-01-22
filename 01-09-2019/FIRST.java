7797bfbba7cb60e17cd425ee71c7e96bf38c566a
sandhya222reddy <sandhyabhairasandram@gmail.com>
2020-01-22 14:41:59 +0530

    Hello

---
+++ 01-09-2019/FIRST.java
@@ -0,0 +1,20 @@
+Class FIRST
+{ 
+    public static void main(String[] args )
+
+    {
+     int a=10;
+     int b=20;
+     int c=30;
+     
+     System.output.print(a);
+     System.output.print(b);
+     System.output.print(c);
+     System.output.print("hello baby")
+
+    }
+
+
+
+
+}
---
+++ 01-09-2019/java.07/suresh.java
@@ -0,0 +1,16 @@
+class abc1223
+
+   { public static void main(String[] args )
+
+    {
+     char a=D;
+     int b=a;
+     int c=30;
+    
+     System.out.println(b);
+     System.out.println(c);
+     System.out.println("hello");
+
+    }
+
+   }
